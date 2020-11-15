bef925eead3b250f9db31f862bdf53a68477a17e
Michael Scherbela <michael.scherbela@gmail.com>
2020-11-15 21:43:59 +0100

    Added catch-all exceptions and restart functionality in case of repeated errors

--- main.py
+++ main.py
@@ -30,16 +30,21 @@ def parseParaglidingStatus(html):
                 return ParagliderStatus.GREEN
             elif 'ampel_gelb' in t:
                 return ParagliderStatus.YELLOW
-            else:
+            elif 'ampel_rot' in t:
                 return ParagliderStatus.RED
-    return ParagliderStatus.ERROR
+            else:
+                return ParagliderStatus.ERROR
 
 def getParaglidingStatus():
-    url = 'http://www.fly-hohewand.at/home/aktuelle-info-ueber-schulbetrieb'
-    r = urequests.get(url)
-    html = r.text
-    r.close()
-    return parseParaglidingStatus(html)
+    try:
+        # url = 'https://static.myjournal.at/flytest.html'
+        url = 'http://www.fly-hohewand.at/home/aktuelle-info-ueber-schulbetrieb'
+        r = urequests.get(url)
+        html = r.text
+        r.close()
+        return parseParaglidingStatus(html)
+    except Exception as e:
+        return ParagliderStatus.ERROR
 
 def connectToWIFI():
     print("Connecting to WIFI...")
@@ -70,6 +75,7 @@ def displayStatusOnLEDs(status):
     elif status == ParagliderStatus.RED:
         LEDS['red'].on()
     else:
+        # ParagliderStatus.ERROR
         LEDS['red'].on()
         LEDS['yellow'].on()
 
@@ -79,15 +85,26 @@ connectToWIFI()
 
 LEDS['red'].off()
 
-while True:
-    status = getParaglidingStatus()
-    displayStatusOnLEDs(status)
-    print(status)
-    if (status == ParagliderStatus.GREEN) or ((status == ParagliderStatus.YELLOW) and wake_on_yellow_switch.value()):
-        buzz(n=3, repeat=2)
-    utime.sleep(5*60)
+error_counter = 0
+try:
+    while True:
+        status = getParaglidingStatus()
+        if status == ParagliderStatus.ERROR:#
+            error_counter += 1
+            if error_counter >= 5:
+                print("Failed 10 times. Resetting controller")
+                machine.reset()
+        displayStatusOnLEDs(status)
+        print(status)
+        if (status == ParagliderStatus.GREEN) or ((status == ParagliderStatus.YELLOW) and wake_on_yellow_switch.value()):
+            buzz(n=3, repeat=2)
+        utime.sleep(60)
+        # Sleep for 60 seconds; On wakeup, the script fill start fully from the beginning, with RAM being fully reset:
+        # The while loop therefore actually runs only once!
+        # Note: This also disables the GPIOs!
+        # machine.deepsleep(60000)
 
-    # Sleep for 60 seconds; On wakeup, the script fill start fully from the beginning, with RAM being fully reset:
-    # The while loop therefore actually runs only once!
-    # Note: This also disables the GPIOs!
-    # machine.deepsleep(60000)
+except Exception as e:
+    print(e)
+    machine.reset()
+
